3633ddddd6a198e89ca0a4a21e7d310090157012
@Criss_UWU <crispaco66@gmail.com>
2026-07-02 17:53:19 -0400

    crear backend Spring Boot

---
+++ src/test/java/com/example/colegio_backend/ColegioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.colegio_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColegioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
