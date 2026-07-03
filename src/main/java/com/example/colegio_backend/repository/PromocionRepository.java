bf89d26aedaae7e57f4cb99ceb4df708b3741fad
@Criss_UWU <crispaco66@gmail.com>
2026-07-02 20:11:47 -0400

    crear crud de promociones

---
+++ src/main/java/com/example/colegio_backend/repository/PromocionRepository.java
@@ -0,0 +1,7 @@
+package com.example.colegio_backend.repository;
+
+import com.example.colegio_backend.Entity.Promocion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PromocionRepository extends JpaRepository<Promocion, Long> {
+}
